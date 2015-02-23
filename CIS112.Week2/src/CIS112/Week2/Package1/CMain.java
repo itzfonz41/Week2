9fdb733e23cf6b7874f73ef88f22f0fd99ad7c9f
Itzfonz41 <alfonsf1@uci.edu>
2015-02-22 22:41:28 -0800

    First Commit package 1

---
+++ CIS112.Week2/src/CIS112/Week2/Package1/CMain.java
@@ -0,0 +1,10 @@
+package CIS112.Week2.Package1;
+
+public class CMain
+{
+	// main method begins execution of Java application
+	public static void main(String[] args)
+	{
+		System.out.println( "Hello, World!" );
+} // end method main
+} // end class HelloWorld
